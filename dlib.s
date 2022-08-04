c7b96156534f24e33c5f2dcc9ad317a4c8ea68d6
Emilio <emiliollbb@gmail.com>
2022-08-04 18:44:00 +0200

    Use arguments in stack

--- dlib.s
+++ dlib.s
@@ -135,6 +135,107 @@ _drawPixelPair_screen: .res 2, $00 ;  Reserve a local zero page pointer for scre
 	LDA (sp), Y
 	STA $df93
 
+	
+
+
+
+	; Initialize screen position
+    LDA #$60
+    STA _drawPixelPair_screen+1
+    LDA #$00
+    STA _drawPixelPair_screen
+        
+    convert_coords_to_mem:
+    LDX #$00
+    ; Load y coord argument in acumulator
+    LDY #$01
+    LDA (sp), Y
+    ; Multiply y coord by 64 (64 bytes each row)
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ASL
+    ; Also shift more sig byte
+    TAY
+    TXA
+    ROL
+    TAX
+    TYA
+    ; Shift less sig byte
+    ; Add to initial memory address, and save it
+    CLC
+    ADC _drawPixelPair_screen
+    STA _drawPixelPair_screen
+
+    ; If overflow, add one to more sig byte
+    BCC conv_coor_mem_01
+    INX
+    conv_coor_mem_01:
+    ; Add calculated offset to $11 (more sig)
+    TXA
+    CLC
+    ADC _drawPixelPair_screen+1
+    STA _drawPixelPair_screen+1
+
+    ; Calculate X coord
+    ; Load y coord
+    LDY #$02
+    LDA (sp), Y
+    ; Divide x coord by 2 (2 pixel each byte)
+    LSR
+    ; Add to memory address
+    CLC
+    ADC _drawPixelPair_screen
+    ; Store in video memory position
+    STA _drawPixelPair_screen
+    ; If overflow, increment left byte
+    BCC conv_coor_mem_02
+    INC _drawPixelPair_screen+1
+    conv_coor_mem_02:
+    ; Store color in accumulator
+    LDY #$00
+    LDA (sp), Y
+    ; Draw actual pixel
+    LDY #$00
+    STA (_drawPixelPair_screen), Y
+
 	; Remove args from stack
 	JSR incsp3
 
